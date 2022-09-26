e37f52394b940bb2539b9c9f32eb0627665eedaf
Irene <irenemanningm@gmail.com>
2022-09-25 20:25:25 -0500

    commit tree 1ddb37c25e8f33d08cf724354eecc9527b521a2c

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,35 @@
 class Movie < ActiveRecord::Base
-
+    def self.create_with_title(title)
+        Movie.create(title: title)
+    end
+    def self.first_movie
+        Movie.first
+    end
+    def self.last_movie
+        Movie.last
+    end
+    def self.movie_count
+        Movie.count
+    end
+    def self.find_movie_with_id(id)
+        Movie.find_by(id: id)
+    end
+    def self.find_movie_with_attributes(a)
+        Movie.find_by(a)
+    end
+    def self.find_movies_after_2002
+        Movie.where("release_date > 2002")
+    end
+    def update_with_attributes(a)
+        self.update(a)
+    end
+    def self.update_all_titles(title)
+        Movie.update(title: title)
+    end
+    def self.delete_by_id(id)
+        Movie.find_by(id: id).destroy
+    end
+    def self.delete_all_movies
+        Movie.destroy_all
+    end
 end
